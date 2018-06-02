eb3da78953490e5c7e0294049ff1052de8c33df9
Alexander <wondroussquirrel@gmail.com>
2018-06-02 15:13:59 +0300

    Delete config.php

--- tutorial_mysql/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	// Определяем глобальные константы ('имя', 'значение')
-	define('ROOT_URL', 'http://localhost/tutorial_mysql/');
-	define('DB_HOST', 'localhost'); 
-	define('DB_USER', 'root');
-	define('DB_PASS', 'sl1pkn0t');
-	define('DB_NAME', 'tutorial');
-?>
